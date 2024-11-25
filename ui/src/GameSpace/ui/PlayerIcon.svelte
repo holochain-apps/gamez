d3e6fd88d07d78ce2bc6d84a9d7505268251a723
Ezequiel Adrian Schwartzman <zequez@gmail.com>
2024-11-25 11:46:54 -0300

    Enhance color contrast for player icon

--- ui/src/GameSpace/ui/PlayerIcon.svelte
+++ ui/src/GameSpace/ui/PlayerIcon.svelte
@@ -9,14 +9,25 @@
   let klass: string = '';
   export { klass as class };
   export let style = '';
+
+  function getContrastingBlackOrWhite(color: string) {
+    const r = parseInt(color.slice(1, 3), 16);
+    const g = parseInt(color.slice(3, 5), 16);
+    const b = parseInt(color.slice(5, 7), 16);
+    const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+    return yiq >= 128 ? '#000' : '#fff';
+  }
+  $: contrastingColor = getContrastingBlackOrWhite(color);
 </script>
 
 <div
-  class={`relative flexcc rounded-full b-black/10 shadow-md text-white b-2 ${klass}`}
+  class={`relative flexcc rounded-full b-black/10 shadow-md b-2 ${klass}`}
   style={`background-color: ${color}; ${style}; height: ${size}px; width: ${size}px; ${style}`}
 >
   {#if pubKey}
     <AgentAvatar class="relative z-20" size={size - 4} {pubKey} />
   {/if}
-  <div class="absolute z-10 inset-0 flexcc mix-blend-difference opacity-50">{slot + 1}</div>
+  <div class="absolute z-10 inset-0 flexcc opacity-80" style="color: {contrastingColor};"
+    >{slot + 1}</div
+  >
 </div>
